b1c9560f9c68f854828ab79ae7ba56448f9e1ed0
Istvan Sajtos <istvan.sajtos@liferay.com>
2013-11-17 23:09:52 +0100

    issue#8 Refactoring Task entity

--- timesheet-portlet/docroot/WEB-INF/src/com/liferay/timesheet/service/impl/TaskServiceImpl.java
+++ timesheet-portlet/docroot/WEB-INF/src/com/liferay/timesheet/service/impl/TaskServiceImpl.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.timesheet.service.impl;
 
 import com.liferay.timesheet.service.base.TaskServiceBaseImpl;
@@ -12,14 +26,14 @@ import com.liferay.timesheet.service.base.TaskServiceBaseImpl;
  * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
  * </p>
  *
- * @author Adorjan
+ * @author Istvan Sajtos
  * @see com.liferay.timesheet.service.base.TaskServiceBaseImpl
  * @see com.liferay.timesheet.service.TaskServiceUtil
  */
 public class TaskServiceImpl extends TaskServiceBaseImpl {
-    /*
-     * NOTE FOR DEVELOPERS:
-     *
-     * Never reference this interface directly. Always use {@link com.liferay.timesheet.service.TaskServiceUtil} to access the task remote service.
-     */
+	/**
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this interface directly. Always use {@link com.liferay.timesheet.service.TaskServiceUtil} to access the task remote service.
+	 */
 }
